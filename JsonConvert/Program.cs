313b81501c07351c7690fe60b6b6ce18e08c0bbb
Dmitry Kovalchuk <55485703+privettalon@users.noreply.github.com>
2021-07-09 11:50:48 +0300

    add minor changes

--- JsonConvert/Program.cs
+++ JsonConvert/Program.cs
@@ -10,17 +10,16 @@ namespace JsonConvert
     {
         static void Main(string[] args)
         {
-            var path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")) +@"JsonExample\test.json";
-            //var a = new CustomConvert(path);
+            //var path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")) +@"JsonExample\test.json";
+            //var path = System.IO.Directory.GetCurrentDirectory() + @"\test2";
 
-            //var res = a.ConvertToDictionary();
-            
-
-
-            var json = new JsonToString(path);
-            
+            Console.WriteLine("Please enter the full directory path");
+            var path = Console.ReadLine();
+            Console.WriteLine("Please enter a file name with an extension");
+            var fileName = Console.ReadLine();
 
 
+            var json = new JsonToString(@path + @"\" + fileName);
             if (json.list != null)
             {
                 foreach (var item in json.list)
--- JsonConvert/newtonsoft/Converter.cs
+++ JsonConvert/newtonsoft/Converter.cs
@@ -5,44 +5,65 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Schema;
+
 
 namespace JsonConvert
 {
     class JsonToString
     {
-        public Dictionary<string,string> list { get; private set; }
+        public Dictionary<string, string> list { get; private set; }
         private JObject json;
+
         public JsonToString(string filePath)
         {
             list = new Dictionary<string, string>();
             string text;
-            
-            using (StreamReader streamreader = new StreamReader(filePath))
+            try
             {
-                text = streamreader.ReadToEnd();
+                using (StreamReader streamreader = new StreamReader(filePath))
+                {
+                    text = streamreader.ReadToEnd();
+                }
+
+                DuplicatePropertyNameHandling duplicateFlag = (DuplicatePropertyNameHandling)2;
+                JsonLoadSettings jsonLoadSettings = (duplicateFlag == DuplicatePropertyNameHandling.Replace)
+                    ? null
+                    : new JsonLoadSettings { DuplicatePropertyNameHandling = duplicateFlag };
+                JObject.Parse(text, jsonLoadSettings);
+
+                json = (JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(text);
+                GetKeyValue(json, "");
             }
-            json = (JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(text);
-            GetKeyValue(json);
+            catch (JsonReaderException)
+            {
+                Console.WriteLine("JSON isn't Valid");
+            }
+            catch(IOException)
+            {
+                Console.WriteLine("\nInput corect directory");
+            }
         }
 
-        private void GetKeyValue(JToken item)
+        private void GetKeyValue(JToken item, string pathString)
         {
-            var temp = item.First;
-            
-            if(temp.HasValues)
-            {
-                GetKeyValue(temp);
-           
-            }
-            else 
-            {
-                list.Add(temp.Path.Replace('.',':'), temp.ToString());
-            }
+            var currentJToken = item.First;
+            var addingPath = currentJToken.Path.Length >= pathString.Length ? new string(currentJToken.Path.Skip(pathString.Length).ToArray()) + ":" : "";
 
-            if (item.Next!=null)
+            pathString += pathString.Length < 1 ? currentJToken.Path + ":" : addingPath;
+
+            if (currentJToken.HasValues)
             {
-                GetKeyValue(item.Next);
+                GetKeyValue(currentJToken, pathString);
+            }
+            else
+            {
+                pathString = pathString.Remove(pathString.Length - 1).Replace(":'", ":").Replace("']", "").Replace("\\'", "'").Replace("['", "");
+                list.Add(pathString, currentJToken.ToString());
+            }
+            if (item.Next != null)
+            {
+                pathString = item.Parent == item.Root ? "" : new string(pathString.Substring(0, item.Parent.Path.Length + 1));
+                GetKeyValue(item.Next, pathString);
             }
         }
     }
